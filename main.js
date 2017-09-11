a6aa8812cfeb697d1cb8638ac47b664aa9918e3b
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-11 13:28:55 +0200

    Init files

---
+++ main.js
@@ -0,0 +1,14 @@
+var result1 = factorial(5);
+console.log(result1);
+
+var result2 = factorial(8);
+console.log(result2);
+
+var result3 = factorial(1);
+console.log(result3);
+
+var result4 = factorial(0);
+console.log(result4);
+
+var result5 = factorial(-1);
+console.log(result5);
